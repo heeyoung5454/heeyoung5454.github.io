63a7a24b829f593d593d4b542acbcfb9f7c91e13
heeyoung5454 <33539970+heeyoung5454@users.noreply.github.com>
2021-01-13 01:13:34 +0900

    faq 테마변경

--- src/views/board/Faq.vue
+++ src/views/board/Faq.vue
@@ -5,7 +5,7 @@
         <h1 class="mb-5">FAQ</h1>
         <p class="text-right text-success"><a href="/contact" v-b-popover.hover.top="'언제든지 연락하실 수 있습니다'">더 궁금하신것이 있다면?</a></p>
         <b-card no-body class="mb-1">
-            <b-button block v-b-toggle.collapse-1 class="text-left color-faq">Q1. 학창시절은 어땠나요?</b-button>
+            <b-button block v-b-toggle.collapse-1 variant="dark" class="text-left">Q1. 학창시절은 어땠나요?</b-button>
             <b-collapse id="collapse-1" visible role="tabpanel">
                 <b-card-body>
                     <b-card-text>{{ text1 }}</b-card-text>
@@ -14,7 +14,7 @@
         </b-card>
 
         <b-card no-body class="mb-1">
-            <b-button block v-b-toggle.collapse-2 class="text-left color-faq">Q2. 전공을 선택한 이유는 무엇인가요?</b-button>
+            <b-button block v-b-toggle.collapse-2 variant="dark" class="text-left color-faq">Q2. 전공을 선택한 이유는 무엇인가요?</b-button>
             <b-collapse id="collapse-2" role="tabpanel">
                 <b-card-body>
                     <b-card-text>{{ text2 }}</b-card-text>
@@ -23,7 +23,7 @@
         </b-card>
 
         <b-card no-body class="mb-1">
-            <b-button block v-b-toggle.collapse-3 class="text-left color-faq">Q3. 관심있는 것은 무엇인가요?</b-button>
+            <b-button block v-b-toggle.collapse-3 variant="dark" class="text-left color-faq">Q3. 관심있는 것은 무엇인가요?</b-button>
             <b-collapse id="collapse-3" role="tabpanel">
                 <b-card-body>
                     <b-card-text>{{ text3 }}</b-card-text>
@@ -32,7 +32,7 @@
         </b-card>
 
         <b-card no-body class="mb-1">
-            <b-button block v-b-toggle.collapse-4 class="text-left color-faq">Q4. 가장 자신있는 기술은?</b-button>
+            <b-button block v-b-toggle.collapse-4 variant="dark" class="text-left color-faq">Q4. 가장 자신있는 기술은?</b-button>
             <b-collapse id="collapse-4" role="tabpanel">
                 <b-card-body>
                     <b-card-text>{{ text4 }}</b-card-text>
@@ -41,7 +41,7 @@
         </b-card>
 
         <b-card no-body class="mb-1">
-            <b-button block v-b-toggle.collapse-5 class="text-left color-faq">Q5. 마지막으로 한마디 하자면?</b-button>
+            <b-button block v-b-toggle.collapse-5 variant="dark" class="text-left color-faq">Q5. 마지막으로 한마디 하자면?</b-button>
             <b-collapse id="collapse-5" role="tabpanel">
                 <b-card-body>
                     <b-card-text>{{ text5 }}</b-card-text>
@@ -72,7 +72,4 @@ export default {
     min-height: 545px;
 }
 
-.color-faq{
-    background-color: gray;
-}
 </style>
